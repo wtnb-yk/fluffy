dcb1a29f0124aef8427a034574a725adb8ba2582
Yuki Watanabe <>
2023-05-05 16:05:36 +0900

    chore: OpenAPI Generator の導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.6"
     id("io.spring.dependency-management") version "1.1.0"
+    id("org.openapi.generator") version "6.5.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
 }
@@ -16,12 +17,37 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    compileOnly("io.swagger.core.v3:swagger-annotations:2.2.8")
+    compileOnly("io.swagger.core.v3:swagger-models:2.2.8")
+    compileOnly("jakarta.annotation:jakarta.annotation-api:2.1.1")
+    compileOnly("jakarta.annotation:jakarta.annotation-api:2.1.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("jakarta.servlet:jakarta.servlet-api:4.0.4")
+    implementation("jakarta.validation:jakarta.validation-api:2.0.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+openApiGenerate {
+    generatorName.set("kotlin-spring")
+    inputSpec.set("$projectDir/_build/openapi.yaml")
+    outputDir.set("$buildDir/openapi/server-code/")
+    apiPackage.set("com.fluffy.openapi.generated.controller")
+    modelPackage.set("com.fluffy.openapi.generated.model")
+    configOptions.set(
+        mapOf(
+            "interfaceOnly" to "true",
+        )
+    )
+    additionalProperties.set(
+        mapOf(
+            "useTags" to "true"
+        )
+    )
 }
 
 tasks.withType<KotlinCompile> {
@@ -33,4 +59,12 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.compileKotlin {
+    dependsOn("openApiGenerate")
+}
+
+kotlin.sourceSets.main {
+    kotlin.srcDir("$buildDir/openapi/server-code/src/main")
 }
