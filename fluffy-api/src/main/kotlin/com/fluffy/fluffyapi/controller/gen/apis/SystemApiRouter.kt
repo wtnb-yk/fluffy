6a3378e42123f5a7b1d90d7fe9c0cb7f2410ad5e
wtnb-yk <theskyisthelimit.0819@gmail.com>
2023-10-08 18:36:19 +0900

    update open-api generate template

--- fluffy-api/src/main/kotlin/com/fluffy/fluffyapi/controller/gen/apis/SystemApiRouter.kt
+++ fluffy-api/src/main/kotlin/com/fluffy/fluffyapi/controller/gen/apis/SystemApiRouter.kt
@@ -15,16 +15,14 @@
 
 package com.fluffy.fluffyapi.controller.gen.apis
 
-import com.fluffy.fluffyapi.controller.system.SystemApiControllerImpl
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.server.coRouter
 
 @Configuration
 class SystemApiRouter {
     @Bean
-    fun SystemApiRoutes(handler: SystemApiControllerImpl) = coRouter {
+    fun SystemApiRoutes(handler: SystemApiController) = coRouter {
         GET("/ping", handler::healthCheck)
     }
 }
