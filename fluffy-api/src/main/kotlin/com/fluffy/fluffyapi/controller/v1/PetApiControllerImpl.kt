d5ccec1fcbfba44d3b50392bbca44aa78947d181
wtnb-yk <theskyisthelimit.0819@gmail.com>
2023-10-09 10:59:49 +0900

    generate get  /pets

--- fluffy-api/src/main/kotlin/com/fluffy/fluffyapi/controller/v1/PetApiControllerImpl.kt
+++ fluffy-api/src/main/kotlin/com/fluffy/fluffyapi/controller/v1/PetApiControllerImpl.kt
@@ -1,4 +1,24 @@
 package com.fluffy.fluffyapi.controller.v1
 
-class PetApiController {
+import com.fluffy.fluffyapi.controller.gen.apis.PetsApiController
+import com.fluffy.fluffyapi.controller.gen.models.Pets
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyValueAndAwait
+
+@Component
+class PetApiControllerImpl: PetsApiController {
+    override suspend fun getPets(request: ServerRequest): ServerResponse {
+        return ServerResponse
+            .ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValueAndAwait(
+                Pets(
+                    id = "6ef0a13b-6b86-48a2-8a4c-2fce55d89844",
+                    name = "daifuku"
+                )
+            )
+    }
 }
--- fluffy-api/src/main/resources/liquibase/sql/2023100902_create_table_pets.sql
+++
@@ -1,7 +0,0 @@
-create table owners (
-                        id UUID NOT NULL DEFAULT gen_random_uuid(),
-                        name varchar(100) NOT NULL,
-                        primary key (id)
-);
-
-insert into owners (name) values ('Mike'), ('Ann');
